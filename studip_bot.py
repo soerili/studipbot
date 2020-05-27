99fb22b6765afe1393d64584ad6843cde1946d3d
soerili <soeren.rempel@web.de>
2020-05-27 18:28:04 +0200

    updated commands.json

--- studip_bot.py
+++ studip_bot.py
@@ -25,9 +25,21 @@ class StudIPBot(discord.Client):
                 command, parameters = message.content.split(' ', 1)
             except ValueError:
                 command, parameters = message.content, None
-            command.pop(0)
-            await COMMANDS.get(command).get('action')(parameters, message, self)
+            command[1:]
+            print(type(message))
+            print(message)
+            for x in message:
+                print(x)
+            #await COMMANDS.get(command).get('action')(parameters, message, self)
+
+
+async def update_command(parameters, client):
+    return None
+
+
+async def forum_command(parameters, client):
+    return None
 
 
 client = StudIPBot()
-client.run('token')
+client.run('')
